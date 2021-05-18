f89c334a25de11902306ef8fb7a7334e6221b2cc
tearsforyears <1027664894@qq.com>
2021-05-18 12:14:05 +0800

    更新redis计数器限流

---
+++ src/main/java/com/example/distcomponents/Limit/RedisRateLimiter.java
@@ -0,0 +1,64 @@
+package com.example.distcomponents.Limit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author zhanghaoyang
+ */
+@Component
+public class RedisRateLimiter {
+    /**
+     * 秒级限制器,实现思路就是每一个固定的秒是redis中的一个key,我们上界是1000次访问
+     * 利用redis充当计数器,其中有两个点需要考虑,redis的
+     * 超过访问直接返回失败,否则则尝试获取
+     */
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+
+    public Boolean tryAcquire() {
+        // 有可能出现多个线程调用,使用ThreadLocal
+        SimpleDateFormat df = ThreadSafeDateFormatter.dateFormatThreadLocal.get();
+        String key = "limit count"+df.format(new Date());
+        long expired = 1000L + new Random().nextInt(1000);
+        // 如果不存在就加入一个key
+        try {
+            redisTemplate.opsForValue().setIfAbsent(key,0L,expired,TimeUnit.MILLISECONDS);
+            // redis实现了一个分布式自增的类
+            RedisAtomicInteger i = new RedisAtomicInteger(key,redisTemplate.getConnectionFactory());
+            int val = i.getAndIncrement();
+            if(val >= 1000){
+                // 超过1000的全部给false,此时数据库的值在增加,但接口全部不予以调用
+                return false;
+            }else{
+                return true;
+            }
+        } catch (Exception e) {
+            // log
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+class ThreadSafeDateFormatter {
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+}
---
+++ src/main/java/com/example/distcomponents/controller/TestController.java
@@ -0,0 +1,42 @@
+package com.example.distcomponents.controller;
+
+import com.example.distcomponents.Limit.RedisRateLimiter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author zhanghaoyang
+ */
+@RestController
+public class TestController {
+    @Autowired
+    RedisRateLimiter limiter;
+    @GetMapping("/Main")
+    public String Main() {
+        // 模拟20个线程不断处理接口,这里用了线程池模拟,本接口仅作为函数入口
+        ExecutorService pool = Executors.newFixedThreadPool(2000);
+        int taskNum = 10000;
+        AtomicInteger in = new AtomicInteger(0);
+        for (int i = 0; i < taskNum; i++) {
+            pool.submit(()->{
+                if(limiter.tryAcquire()){
+                    System.out.println("放行调用接口");
+                    // 调用接口
+//                    System.out.println(in.getAndIncrement());
+                    System.out.println(Thread.currentThread().getName()+"调用接口");
+                }else{
+//                    System.out.println(in.get()+"refuse");
+                    System.out.println(Thread.currentThread().getName()+"限制访问,进行服务降级处理");
+                }
+            });
+        }
+        return "ok";
+    }
+}
